3e19e98b941eb82e7732afacaf8112bad9ab6bc6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2013-12-07 12:40:27 +0900

    add support for EFW transaction response in monitor.c
    As a sample.

--- monitor.c
+++ monitor.c
@@ -9,6 +9,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <endian.h>
+#include <time.h>
 #include <sys/ioctl.h>
 #include <sys/epoll.h>
 #include <linux/firewire-cdev.h>
@@ -17,10 +18,11 @@
 
 static struct snd_firewire_get_info hwdep_info;
 bool try_lock;
+bool fireworks = false;
 
 static void print_event_lock(struct snd_firewire_event_lock_status *msg, int fd)
 {
-	printf("Lock Status:\n");
+	printf("\nLock Status:\n");
 	printf("Status:\t%s\n", msg->status ? "Locked" : "Unlocked" );
 
 	if (try_lock) {
@@ -34,15 +36,47 @@ static void print_event_lock(struct snd_firewire_event_lock_status *msg, int fd)
 
 static void print_event_dice(struct snd_firewire_event_dice_notification *msg)
 {
-	printf("Dice Norification:\n");
+	printf("\nDice Norification:\n");
 	printf("Notification: 0x%x\n", msg->notification);
 	return;
+}
+
+static void print_event_efw(struct snd_firewire_event_efw_response *evt, int count)
+{
+	uint32_t *resp;
+	struct snd_efw_transaction *t;
+	unsigned int i, index, length, params;
+
+	resp = evt->response;
+	count /= 4;
+
+	index = 0;
+	while (count > 0) {
+		t = (struct snd_efw_transaction *)resp;
+		length = be32toh(t->length);
+
+		printf("\nEFW Response %d:\n", index++);
+		printf("Length:\t\t%d\n", be32toh(t->length));
+		printf("Version:\t%d\n", be32toh(t->version));
+		printf("Seqnum:\t\t%d\n", be32toh(t->seqnum));
+		printf("Category:\t%d\n", be32toh(t->category));
+		printf("Command:\t%d\n", be32toh(t->command));
+		printf("Status:\t\t%d\n", be32toh(t->status));
+
+		params = length - sizeof(struct snd_efw_transaction) / sizeof(uint32_t);
+		if (params > 0)
+			for (i = 0; i < params; i++)
+				printf("params[%d]:\t%08X\n", i, be32toh(t->params[i]));
+
+		resp += length;
+		count -= length * sizeof(uint32_t);
+	}
 }
 
 static int read_event(int fd)
 {
 	int count;
-	char buf[1024];
+	char buf[1024] = {0};
 	struct snd_firewire_event_common *event;
 
 	count = read(fd, buf, 1024);
@@ -50,18 +84,43 @@ static int read_event(int fd)
 		return -1;
 
 	event = (struct snd_firewire_event_common *)buf;
-	if (event->type = SNDRV_FIREWIRE_EVENT_LOCK_STATUS)
+	if (event->type == SNDRV_FIREWIRE_EVENT_LOCK_STATUS)
 		print_event_lock((struct snd_firewire_event_lock_status *)buf, fd);
 	else if (event->type == SNDRV_FIREWIRE_EVENT_DICE_NOTIFICATION)
 		print_event_dice((struct snd_firewire_event_dice_notification *)buf);
+	else if (event->type == SNDRV_FIREWIRE_EVENT_EFW_RESPONSE)
+		print_event_efw((struct snd_firewire_event_efw_response *)buf, count);
 
 	return 0;
 }
 
+static int write_event(int fd)
+{
+	int count;
+	struct snd_efw_transaction *t;
+	char buf[1024] = {0};
+
+	t = (struct snd_efw_transaction *)buf;
+	t->length	= htobe32(6);
+	t->version	= htobe32(1);
+	t->seqnum	= htobe32(0);
+	t->category	= htobe32(3);
+	t->command	= htobe32(5);
+	t->status	= htobe32(0);
+
+	count = write(fd, buf, sizeof(struct snd_efw_transaction));
+	if (count < 0)
+		printf("err: %d\n", count);
+
+	return count;
+}
+
 #define EVENTS 10
+time_t cmd_deffer = 0;
 static int main_loop(int fd)
 {
 	int epfd, count, i, err;
+	time_t now;
 	struct epoll_event ev, events[EVENTS];
 
 	epfd = epoll_create(EVENTS);
@@ -74,6 +133,9 @@ static int main_loop(int fd)
 	memset(&ev, 0, sizeof(struct epoll_event));
 	ev.events = EPOLLIN;
 	ev.data.fd = fd;
+
+	if (fireworks)
+		ev.events |= EPOLLOUT;
 
 	if (epoll_ctl(epfd, EPOLL_CTL_ADD, fd, &ev) < 0)
 		return errno;
@@ -88,9 +150,22 @@ static int main_loop(int fd)
 		}
 
 		for (i = 0; i < count; i++) {
-			if (read_event(events[i].data.fd) < 0) {
-				err = errno;
-				break;
+			if (events[i].events & EPOLLOUT) {
+				if (time(&now) < 0)
+					break;
+				if (now > cmd_deffer) {
+					if (write_event(events[i].data.fd) < 0) {
+						err = errno;
+						break;
+					}
+					cmd_deffer = now + 3;
+				}
+			}
+			if (events[i].events & EPOLLIN) {
+				if (read_event(events[i].data.fd) < 0) {
+					err = errno;
+					break;
+				}
 			}
 		}
 	}
@@ -127,6 +202,9 @@ int main(int argc, void *argv[])
 	       hwdep_info.guid[3], hwdep_info.guid[4], hwdep_info.guid[5],
 	       hwdep_info.guid[6], hwdep_info.guid[7] );
 	printf("Name: %s\n\n", hwdep_info.device_name);
+
+	if (hwdep_info.type == SNDRV_FIREWIRE_TYPE_FIREWORKS)
+		fireworks = true;
 
 	err = main_loop(fd);
 	close(fd);
