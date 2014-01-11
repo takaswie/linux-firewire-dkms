0aa3509aeffe1ac531e4ed3f7508fe702aad27c6
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-01-11 13:12:27 +0900

    oxfw: Add some AV/C commands to get stream formation and supported sampl
    OXFW970/971 may supports AV/C Stream Format Information Specification 1.1
    Working Draft (Apr 2005, 1394TA). So this driver uses 'EXTENDED STREAM FORMA
    INFORMATION' command.
    
    This command has two subfunctions, 'SINGLE' and 'LIST'. Drivers can use
    'SINGLE' subfunction to know current formation of AMDTP stream, Drivers can
    use 'LIST' subfunction to know an available formation of AMDTP stream in a
    certain sampling rate. Followed commit adds parser for returned information.
    
    This commit adds 'inquiry' version of 'PLUG SIGNAL FORMAT' command. When
    device doesn't support 'LIST' subfunction of 'AV/C Stream Format Information
    command, drivers can use 'inquiry' command to get supported sampling rates.

--- sound/firewire/oxfw/oxfw.h
+++ sound/firewire/oxfw/oxfw.h
@@ -57,6 +57,36 @@ struct snd_oxfw {
 	s16 volume_max;
 };
 
+/* AV/C Stream Format Information Specification 1.1 (Apr 2005, 1394TA) */
+#define AVC_GENERIC_FRAME_MAXIMUM_BYTES	512
+int avc_stream_get_format(struct fw_unit *unit,
+			  enum avc_general_plug_dir dir, unsigned int pid,
+			  u8 *buf, unsigned int *len,
+			  unsigned int eid);
+static inline int
+avc_stream_get_format_single(struct fw_unit *unit,
+			     enum avc_general_plug_dir dir, unsigned int pid,
+			     u8 *buf, unsigned int *len)
+{
+	return avc_stream_get_format(unit, dir, pid, buf, len, 0xff);
+}
+static inline int
+avc_stream_get_format_list(struct fw_unit *unit,
+			   enum avc_general_plug_dir dir, unsigned int pid,
+			   u8 *buf, unsigned int *len,
+			   unsigned int eid)
+{
+	return avc_stream_get_format(unit, dir, pid, buf, len, eid);
+}
+
+/*
+ * AV/C Digital Interface Command Set General Specification 4.2
+ * (Sep 2004, 1394TA)
+ */
+int avc_general_inquiry_sig_fmt(struct fw_unit *unit, unsigned int rate,
+				enum avc_general_plug_dir dir,
+				unsigned short pid);
+
 int snd_oxfw_stream_init(struct snd_oxfw *oxfw);
 int snd_oxfw_stream_start(struct snd_oxfw *oxfw, unsigned int rate);
 void snd_oxfw_stream_stop(struct snd_oxfw *oxfw);
