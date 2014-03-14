677448c5be860444ae8dfa96bc186286e15c2f6a
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-03-14 13:17:43 +0900

    firewire-lib/fireworks: Add a quirk for wrong dbs in tx packets
    One of Fireworks firmware, named  as 'AudioFire9', seems to transmit
    packets with wrong value of dbs. It's always 0x11 but actual size of
    data block is different.
    
    This commit adds a flag for this quirk and some codes to calculate
    correct size.

--- sound/firewire/fireworks/fireworks.c
+++ sound/firewire/fireworks/fireworks.c
@@ -214,6 +214,8 @@ efw_probe(struct fw_unit *unit,
 	err = get_hardware_info(efw);
 	if (err < 0)
 		goto error;
+	if (entry->model_id == MODEL_ECHO_AUDIOFIRE_9)
+		efw->is_af9 = true;
 
 	err = snd_efw_stream_init_duplex(efw);
 	if (err < 0)
