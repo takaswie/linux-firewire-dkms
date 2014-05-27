331a7919b3c4b3ce1664b03ccc3587fb1cafab8d
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-05-28 00:19:26 +0900

    firewire-lib: Use ideom to assignen __be32 value to bool value

--- sound/firewire/cmp.c
+++ sound/firewire/cmp.c
@@ -164,12 +164,10 @@ int cmp_connection_check_used(struct cmp_connection *c, bool *used)
 	err = snd_fw_transaction(
 			c->resources.unit, TCODE_READ_QUADLET_REQUEST,
 			pcr_address(c), &pcr, 4, 0);
-	if (err >= 0) {
-		if ((pcr & cpu_to_be32(PCR_BCAST_CONN | PCR_P2P_CONN_MASK)) > 0)
-			*used = true;
-		else
-			*used = false;
-	}
+	if (err >= 0)
+		*used = !!(pcr & cpu_to_be32(PCR_BCAST_CONN |
+					     PCR_P2P_CONN_MASK));
+
 	return err;
 }
 EXPORT_SYMBOL(cmp_connection_check_used);
