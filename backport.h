59d58c94491497776a57e8711dbf2062a0ea6c95
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2014-10-22 22:55:03 +0900

    enable to be built in Linux 3.16 or later

--- backport.h
+++ backport.h
@@ -6,6 +6,8 @@
 void fw_schedule_bus_reset(struct fw_card *card, bool delay, bool short_reset);
 
 /* commit 393aa9c1cc514774332d7bc861307a76206e358d */
+/* This macro is just convinient to detect Linux 3.16 or later. */
+#ifndef dev_to_snd_card
 static inline int snd_card_new(struct device *parent, int idx, const char *xid,
 			       struct module *module, int extra_size,
 			       struct snd_card **card_ret)
@@ -15,6 +17,7 @@ static inline int snd_card_new(struct device *parent, int idx, const char *xid,
 		(*card_ret)->dev = parent;
 	return err;
 }
+#endif
 
 /* commit 16735d022f72b20ddbb2274b8e109f69575e9b2b */
 #ifdef INIT_COMPLETION
