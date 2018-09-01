3da71609a9b2d0351957dac33988c5b421926abe
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-09-01 11:35:46 +0900

    liquidmix: add skeleton for Focusrite Liquid Mix series
    This commit adds a new driver for Focusrite Liquid Mix series. This commit
    just handle supported unit on IEEE 1394 bus and nothing to do.
    
    Focusrite ships Liquid Mix 16 and 32 in 2006. The models consist of below
    hardware chips:
     - Texus Instruments TSB41AB1
     - Texus Instruments TSB12LVD1B
     - Altera MAX II

---
+++ sound/firewire/liquidmix/lm.c
@@ -0,0 +1,68 @@
+/*
+ * lm.c - a part of driver for Focusrite Liquid Mix series
+ *
+ * Copyright (c) 2015-2017 Takashi Sakamoto <o-takashi@sakamocchi.jp>
+ *
+ * Licensed under the terms of the GNU General Public License, version 2.
+ */
+
+#include "lm.h"
+
+#define OUI_FOCUSRITE	0x00130e
+
+MODULE_DESCRIPTION("Focusrite Liquid Mix driver");
+MODULE_AUTHOR("Takashi Sakamoto <o-takashi@sakamocchi.jp>");
+MODULE_LICENSE("GPL v2");
+
+#define LM_DEVICE_ENTRY(model)					\
+	{							\
+		.match_flags = IEEE1394_MATCH_VENDOR_ID |	\
+			       IEEE1394_MATCH_MODEL_ID,		\
+		.vendor_id = OUI_FOCUSRITE,			\
+		.model_id = (model),				\
+	}
+
+static const struct ieee1394_device_id lm_id_table[] = {
+	{},
+};
+MODULE_DEVICE_TABLE(ieee1394, lm_id_table);
+
+static int lm_probe(struct fw_unit *unit,
+		    const struct ieee1394_device_id *entry)
+{
+	return 0;
+}
+
+static void lm_remove(struct fw_unit *unit)
+{
+}
+
+static void lm_bus_update(struct fw_unit *unit)
+{
+	return;
+}
+
+static struct fw_driver lm_driver = {
+	.driver   = {
+		.owner	= THIS_MODULE,
+		.name	= KBUILD_MODNAME,
+		.bus	= &fw_bus_type,
+	},
+	.probe    = lm_probe,
+	.update   = lm_bus_update,
+	.remove   = lm_remove,
+	.id_table = lm_id_table,
+};
+
+static int __init alsa_lm_init(void)
+{
+	return driver_register(&lm_driver.driver);
+}
+
+static void __exit alsa_lm_exit(void)
+{
+	driver_unregister(&lm_driver.driver);
+}
+
+module_init(alsa_lm_init);
+module_exit(alsa_lm_exit);
---
+++ sound/firewire/liquidmix/lm.h
@@ -0,0 +1,23 @@
+/*
+ * lm.h - a part of driver for Focusrite Liquid Mix series
+ *
+ * Copyright (c) 2018 Takashi Sakamoto <o-takashi@sakamocchi.jp>
+ *
+ * Licensed under the terms of the GNU General Public License, version 2.
+ */
+
+#ifndef SOUND_FIREWIRE_LIQUIDMIX_H_INCLUDED
+#define SOUND_FIREWIRE_LIQUIDMIX_H_INCLUDED
+
+#include <linux/device.h>
+#include <linux/firewire.h>
+#include <linux/firewire-constants.h>
+#include <linux/module.h>
+#include <linux/mod_devicetable.h>
+#include <linux/mutex.h>
+#include <linux/slab.h>
+
+/* TODO: remove when merging to upstream. */
+#include "../../../backport.h"
+
+#endif
