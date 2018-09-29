03b9ded7258bdf27c3cb6133add86ca3fb886703
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2018-09-29 16:54:00 +0900

    simplify backport.h

--- backport.h
+++ backport.h
@@ -1,15 +1,3 @@
-#include <target/target_core_base.h>
-#include <target/target_core_backend.h>
-
-#ifdef TRANSPORT_FLAG_PASSTHROUGH_ALUA
-#include <linux/sched/signal.h>
-#else
-#include <linux/sched.h>
-#endif
-
 #include "./include/uapi/sound/firewire.h"
 #include "./include/uapi/sound/asound.h"
 #include "./include/uapi/sound/tlv.h"
-
-#include <sound/core.h>
-#include <sound/pcm.h>
