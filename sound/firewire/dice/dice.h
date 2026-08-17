aed07b675dc1c7d2334318e6767cb3f4eea973f8
Takashi Sakamoto <o-takashi@sakamocchi.jp>
2026-08-17 22:31:29 +0900

    Revert "ALSA: firewire: Drop redundant mod_devicetable.h includes"
    This reverts commit 20e93c7c6f620f6cbc59aaa9d594ecf14fdfadba.

--- sound/firewire/dice/dice.h
+++ sound/firewire/dice/dice.h
@@ -17,6 +17,7 @@
 #include <linux/firewire-constants.h>
 #include <linux/jiffies.h>
 #include <linux/module.h>
+#include <linux/mod_devicetable.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
 #include <linux/spinlock.h>
--- sound/firewire/oxfw/oxfw.h
+++ sound/firewire/oxfw/oxfw.h
@@ -9,6 +9,7 @@
 #include <linux/firewire.h>
 #include <linux/firewire-constants.h>
 #include <linux/module.h>
+#include <linux/mod_devicetable.h>
 #include <linux/mutex.h>
 #include <linux/slab.h>
 #include <linux/compat.h>
